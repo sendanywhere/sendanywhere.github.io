2d2a36fffcdd9c260aab8982ca20cec42a6a257c
Q2 <slowoasis@gmail.com>
2015-01-14 18:00:41 +0900

    update widget dropzone

--- js/main.js
+++ js/main.js
@@ -33,25 +33,5 @@ $(document).ready(function() {
 	});
 
 
-	//open interest point description
-	$('.single-point').children('a').on('click', function(){
-		var selectedPoint = $(this).parent('li');
-		if( selectedPoint.hasClass('is-open') ) {
-			selectedPoint.removeClass('is-open').addClass('visited');
-		} else {
-			selectedPoint.addClass('is-open').siblings('.single-point.is-open').removeClass('is-open').addClass('visited');
-		}
-	});
-	//close interest point description
-	$('.cd-close-info').on('click', function(event){
-		event.preventDefault();
-		$(this).parents('.single-point').eq(0).removeClass('is-open').addClass('visited');
-	});
-
-
-
-
-
-
 });
 
